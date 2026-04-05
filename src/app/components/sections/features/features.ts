61b151fccf129368078b4b310d5796302a5b9076
Jimmy Ren <jimmyren@Jimmys-MacBook-Pro.local>
2026-04-05 19:34:16 -0400

    updated, hero, features, and metrics page with skeleton divs

--- src/app/components/sections/features/features.ts
+++ src/app/components/sections/features/features.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-features',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './features.html',
 })
-export class FeaturesComponent {}
+export class FeaturesComponent {
+  features = [
+    {featureName: "Feature 1"},
+    {featureName: "Feature 2"},
+    {featureName: "Feature 3"},
+    {featureName: "Feature 4"}
+  ]
+
+
+}
